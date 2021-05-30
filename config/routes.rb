2dce5621178000d57874a81b5e47ba584d59b398
mirion615 <mirion615@gmail.com>
2021-05-30 09:24:31 +0900

    axiosの導入

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root "quizzes#index"
   resources :quizzes
+
+  namespace :api, { format: 'json'} do
+    namespace :v1 do
+      resources :quizzes
+    end
+  end
 end
