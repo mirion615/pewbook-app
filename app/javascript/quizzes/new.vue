a487a6784d7afccaf2d32319b8f952bf07f95b27
mirion615 <mirion615@gmail.com>
2021-06-03 17:34:39 +0900

    修正

--- app/javascript/quizzes/new.vue
+++ app/javascript/quizzes/new.vue
@@ -1,16 +1,6 @@
 <template>
-<<<<<<< Updated upstream:app/javascript/quizzes/new.vue
-  <div id="quizTable">
-    <el-row>
-      <el-button
-        icon="el-icon-plus"
-        @click="createQuizDialog = true"
-        circle></el-button>
-      <el-col :span="12" :offset="6">
-=======
   <div class="quiz__table">
     <el-input v-model="search" size="mini" class="input" placeholder="英単語を検索"/>
->>>>>>> Stashed changes:app/javascript/quizzes/word.vue
         <el-table
           :data="quizzes.filter(
             data => !search || data.question.includes(search))"
@@ -31,19 +21,6 @@
             label="Answer Description">
           </el-table-column>
         </el-table>
-<<<<<<< Updated upstream:app/javascript/quizzes/new.vue
-      </el-col>
-    </el-row>
-    <el-dialog 
-      :visible.sync="createQuizDialog"
-      width="30%"
-      center>
-      <quiz-form 
-      @close="closeDialog"
-      @add="addQuiz"></quiz-form>
-    </el-dialog>
-=======
->>>>>>> Stashed changes:app/javascript/quizzes/word.vue
   </div>
 </template>
 
@@ -54,34 +31,14 @@ export default {
   data() {
     return {
       quizzes: [],
-<<<<<<< Updated upstream:app/javascript/quizzes/new.vue
-      createQuizDialog: false,
-      }
-  },
-  components: {
-    QuizForm,
-=======
       search:''
     }
->>>>>>> Stashed changes:app/javascript/quizzes/word.vue
   },
   created() {
     axios.get('/api/v1/quizzes/new').then((res) => {
       this.quizzes = res.data;
     });
   },
-<<<<<<< Updated upstream:app/javascript/quizzes/new.vue
-  methods: {
-    closeDialog() {
-      this.createQuizDialog = false;
-    },
-    addQuiz(quiz) {
-      this.quizzes.push(quiz);
-    },
-  },
-};
-</script>
-=======
 }
 </script>
 
@@ -102,4 +59,3 @@ export default {
   width: 30%;
 }
 </style>
->>>>>>> Stashed changes:app/javascript/quizzes/word.vue
