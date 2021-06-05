d27cb62293cccb4e9712483cfd4885ed5c881cf7
mirion615 <mirion615@gmail.com>
2021-06-05 15:12:55 +0900

    修正

--- db/migrate/20210527092222_create_quizzes.rb
+++ db/migrate/20210527092222_create_quizzes.rb
@@ -6,6 +6,7 @@ class CreateQuizzes < ActiveRecord::Migration[6.0]
       t.string :incorrect1,         null: false
       t.string :incorrect2,         null: false
       t.string :answer_description, null: false
+      t.integer :category_id,       null: false
       t.references :user,           foreign_key: true
       t.timestamps
     end
