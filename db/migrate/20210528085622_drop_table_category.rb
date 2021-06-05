875a64d70f90e7d1842cc4de04713cddd9968ebb
mirion615 <mirion615@gmail.com>
2021-06-05 13:32:44 +0900

    ファイル削除

--- db/migrate/20210528085622_drop_table_category.rb
+++
@@ -1,8 +0,0 @@
-class DropTableCategory < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :categories do |t|
-      t.string :name, null: false
-      t.timestamps null: false
-    end
-  end
-end
