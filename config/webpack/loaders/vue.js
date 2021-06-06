6c6c9abb1257c077f495a091ab37f8067ff9db16
mirion615 <mirion615@gmail.com>
2021-06-06 10:50:33 +0900

    修正

--- config/webpack/loaders/vue.js
+++ config/webpack/loaders/vue.js
@@ -2,5 +2,12 @@ module.exports = {
   test: /\.vue(\.erb)?$/,
   use: [{
     loader: 'vue-loader'
-  }]
+  }],
+  
+  configureWebpack: {
+    performance: {
+      maxAssetSize: 270000,
+      maxEntrypointSize: 700000,
+    }
+  } 
 }
