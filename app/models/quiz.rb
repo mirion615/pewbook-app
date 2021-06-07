df97780a8e518f6fe5c5532f9b7496c17248617f
mirion615 <mirion615@gmail.com>
2021-06-07 15:51:16 +0900

    クイズモデル

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,13 +1,14 @@
 class Quiz < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :user, optional: true
 
   with_options presence: true do
     validates :question
     validates :correct
     validates :incorrect1
     validates :incorrect2
-    validates :answer_description
+    validates :answer_description, length: { maximum: 1_000 }
     
     with_options numericality: { other_than: 1 } do
       validates :category_id
