fe509bca6b1b81c4012e4bc52417013fcd3ebb33
mirion615 <mirion615@gmail.com>
2021-06-07 11:20:40 +0900

    修正

--- app/javascript/packs/quizzes/user_words.js
+++ app/javascript/packs/quizzes/user_words.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import UserWords from '../../quizzes/user_words.vue'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
-import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI, { locale })
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
