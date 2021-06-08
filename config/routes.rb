0f4a6ad2fc39ff27cfe6268b8fc4e490b247c059
mirion615 <mirion615@gmail.com>
2021-06-08 11:42:28 +0900

    ルーティング、コントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "quizzes#index"
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  
   resources :users, only: [:show]
   resources :quizzes do
     collection do
