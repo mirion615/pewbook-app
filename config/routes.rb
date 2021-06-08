39b8435e3d7c29254eca3eebb2d6ed6c0b6a818a
mirion615 <mirion615@gmail.com>
2021-06-08 12:11:40 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root "quizzes#index"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  
+
   resources :users, only: [:show]
   resources :quizzes do
     collection do
