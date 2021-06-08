82750729838a6685a800e04ce4bcacca6e40b6f7
mirion615 <mirion615@gmail.com>
2021-06-08 12:11:27 +0900

    間違い

--- app/controllers/users/session.controller.rb
+++
@@ -1,6 +0,0 @@
-class Users::SessionsController < Devise::SessionsController
-  def guest_sign_inuser = User.guest_sign_in
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
-end
