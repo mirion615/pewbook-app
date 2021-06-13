c136d2641c45527da242308cae1e962cbd5aaefd
mirion615 <mirion615@gmail.com>
2021-06-13 18:08:08 +0900

    試しに追記

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../packs/preview")
-require("../quizzes/form")
-require("../quizzes/user_words")
-require("../quizzes/word")
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
