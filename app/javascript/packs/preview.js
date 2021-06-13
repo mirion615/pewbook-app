893c816255bf3b2cd3ba6f4173351c245e01426d
mirion615 <mirion615@gmail.com>
2021-06-13 21:44:27 +0900

    previewの一部修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -3,8 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const AvatarList = document.getElementById('avatar-list');
 
     const createImageHTML = (blob) => {
-      const imageElement = document.createElement('div')
-      const blobImage = document.createElement('img')
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
       blobImage.setAttribute('src', blob);
       imageElement.appendChild(blobImage);
       AvatarList.appendChild(imageElement);
