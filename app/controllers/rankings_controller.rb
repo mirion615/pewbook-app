745862fa990d66a3edec4540793989f34ff6bc9b
mirion615 <mirion615@gmail.com>
2021-06-15 16:55:10 +0900

    コントローラー追加

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,5 +1,8 @@
 class RankingsController < ApplicationController
 
+  def index
+    @ranking_data = Ranking.joins(:user).limit(10).order('sum_percentage_correct_answer DESC').group(:nickname).sum(:percentage_correct_answer)
+  end
   def create
     score = Ranking.new(ranking_params)
     score.save
