1e09a430d13e9b067229b6a15d6e116be6226a66
mirion615 <mirion615@gmail.com>
2021-07-03 21:37:36 +0900

    nicknameのアップデート

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,9 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: [:update, :destroy]
+  before_action :configure_account_update_params, only: [:update]
 
   def ensure_normal_user
     if resource.email == 'guest@example.com'
       redirect_to root_path, alert:'ゲストユーザーの更新・削除はできません'
     end
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 end
