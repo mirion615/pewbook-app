f94bfb1cc0a1c828db93a26199e50cfba3db9300
mirion615 <mirion615@gmail.com>
2021-07-13 19:13:42 +0900

    rankingコントローラーにコメント

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,9 +1,12 @@
 class RankingsController < ApplicationController
 
   def index
+    # userモデルとつなげて、合計点数が一番多いuser_idからnicknameを取得する
     @ranking_data = Ranking.joins(:user).limit(10).order('sum_percentage_correct_answer DESC').group(:nickname).sum(:percentage_correct_answer)
   end
+
   def create
+    # 問題1セットを時終わる毎に点数を送信
     score = Ranking.new(ranking_params)
     score.save
     redirect_to form_quizzes_path
