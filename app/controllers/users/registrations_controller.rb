249604f6774bb7297106a3130a36d5f004ae3a9f
mirion615 <mirion615@gmail.com>
2021-07-13 19:17:24 +0900

    registrationコントローラーにコメント

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,16 +2,19 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: [:update, :destroy]
   before_action :configure_account_update_params, only: [:update]
 
+  # ゲストユーザーの編集削除はできないようにする
   def ensure_normal_user
     if resource.email == 'guest@example.com'
       redirect_to root_path, alert:'ゲストユーザーの更新・削除はできません'
     end
   end
 
+  # アカウント編集時に、nicknameが通るようにする
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
 
+  # アカウント編集時に、パスワード無しで通るようにする
   def update_resource(resource, params)
     resource.update_without_password(params)
   end
